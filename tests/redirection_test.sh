ffd50b4566a14b6a563d10fb021249cabbb2202c
Dillon Sio <dsio001@hammer.cs.ucr.edu>
2016-06-07 22:00:49 -0700

    Added test cases for redirection

---
+++ tests/redirection_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat < LICENSE > test.txt
+
+cat LICENSE
+
+cat > hello.txt
+
+ls > filelist.txt
+
+ls >> filelist.txt
+
+sort < filelist.txt
+
+sort < filelist.txt > sortedfilelist.txt
+
+
