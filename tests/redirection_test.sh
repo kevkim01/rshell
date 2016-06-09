eb039d4a0f1671f1d081b7f03bc2d063ef320f27
Dillon Sio <dsio001@hammer.cs.ucr.edu>
2016-06-08 23:05:38 -0700

    Added test caese with pipe and redirection

--- tests/redirection_test.sh
+++ tests/redirection_test.sh
@@ -15,3 +15,9 @@ sort < filelist.txt
 sort < filelist.txt > sortedfilelist.txt
 
 cat README.md | tr a-z A-Z
+
+cat < README.md | tr a-z A-Z
+
+cat README.md | tee newOutputFile
+
+tr a-z A-Z > newOutputFile2
